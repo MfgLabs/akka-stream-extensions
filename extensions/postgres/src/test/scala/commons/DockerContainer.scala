c4ef9a4ba221efe29b9ed665c88e2f3bd05717bb
damien <dmnpignaud@gmail.com>
2015-07-15 14:26:24 +0200

    merged with master

--- extensions/postgres/src/test/scala/commons/DockerContainer.scala
+++ extensions/postgres/src/test/scala/commons/DockerContainer.scala
@@ -21,32 +21,17 @@ trait DockerContainer extends BeforeAndAfterEach {
   def hostConfig: HostConfig
 
   val docker = tugboat.Docker()
-
   var container: Option[String] = None
 
   def dockerIp = {
-    println("//////////////////////////////////////////////")
-    println(docker.hostStr)
-    println(docker.host)
-    println((docker.host / "TOTO").toRequestBuilder.build().getURI)
-    if (docker.hostStr.startsWith("https")){
-      val reg = """http[s]*://(.*):.*""".r
-      val reg(ip) = docker.hostStr
-      println("IP : "+ ip)
-      ip
-    } else {
-      //"localhost" //tcp://127.0.0.1:4243"//
-      "unix:///var/run/docker.sock"
-    }
+    val reg = """https://(.*):.*""".r
+    val reg(ip) = docker.hostStr
+    ip
   }
 
   override protected def beforeEach(): Unit = {
     val f = for {
       c <- docker.containers.Create(containerConfig, Some(containerName))()
-      _ = println("//////////////////////////////////////////////")
-      _ = println(docker.hostStr)
-      _ = println(docker.host)
-      _ = println(docker.host / "TOTO")
       _ <- docker.containers.get(c.id).Start(hostConfig)()
     } yield c.id
     container = Some(Await.result(f, timeout))
